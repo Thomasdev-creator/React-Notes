1e4e5198a831fd23db4705282c9fb9831a08cbc6
Thomas Fournier <thomasfournierbrest@gmail.com>
2023-09-07 16:51:32 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import NotesList from "./components/NotesList"
+import { useSelector, useDispatch } from "react-redux"
+import { getNotesFromApi } from "./features/notes";
+import Sidebar from "./components/Sidebar";
+import SideNotes from "./components/SideNotes"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  const dispatch = useDispatch()
+  const notes = useSelector(state => state.notes)
+  console.log(notes);
+  if(!notes.list) {
+    dispatch(getNotesFromApi())
+  }
+
+  return (
+    <div className="bg-slate-800 min-h-screen flex">
+      <BrowserRouter>
+      <Sidebar />
+      <SideNotes />
+
+      <Routes>
+        <Route path="/" element={<NotesList />}/>
+      </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
